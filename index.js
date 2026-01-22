38b5891d8b1d6078f4a4d4c92bdfbbd8008ab520
segundocristian219-oss <segundocristian219@gmail.com>
2026-01-22 13:36:35 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -324,7 +324,7 @@ async function connectionUpdate(update) {
       try {
         const data = JSON.parse(fs.readFileSync(restarterFile, "utf-8"))
         if (data.chatId) {
-          await conn.sendMessage(data.chatId, { text: "✅ *𝐅𝐎𝐗 𝐁𝐀𝐋𝐀 𝐁𝐎𝐓 está en línea nuevamente* 🚀" })
+          await conn.sendMessage(data.chatId, { text: "✅ *𝐅𝐑𝐄𝐒𝐈𝐓𝐀 𝐁𝐎𝐓 está en línea nuevamente* 🚀" })
           console.log(chalk.yellow("📢 Aviso enviado al grupo del reinicio."))
           fs.unlinkSync(restarterFile)
         }
